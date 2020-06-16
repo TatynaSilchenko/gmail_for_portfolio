23687450b94ecb43745502c7e2cb0b3b4a50feed
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-06-16 16:07:03 +0300

    fix: fix env variables

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 
 const app = express();
