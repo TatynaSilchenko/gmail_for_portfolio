1bbfd2c1074b9401e2644101d42eca48149da782
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-07-06 15:14:59 +0300

    fix: fix env variables

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
 
 const app = express();
 let login = process.env.SMTP_LOGIN||"...";
 let password = process.env.SMTP_PASSWORD||"...";
+// let port = process.env.PORT||3010;
+let port = 3010;
 
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 let transporter = nodemailer.createTransport({
-    service: 'smtp.gmail.com',
+    // service: 'smtp.gmail.com',
+    service: 'gmail',
     auth: {
-        user: 'taniasilchenko212@gmail.com', // generated ethereal user
+        // type:"OAuth2",
+        user: "taniasilchenko212@gmail.com", // generated ethereal user
+        // accessToken:"ya29.a0AfH6SMDkjVwZEdSRd4Sh3WRPf3Q4N_umr99Un8FSRmzD9gSlhXsViga52spt2NTOpryy43TY_FU3sMB41fhtnwbDR3vvxvR2Ba3wxaGSM1dNvDY-ps3sJHPte2rabGypCfhb6JQ2C_ag-HqSXDqbby5ifsKVRCEBuik",
+        // expires:1592570987208+60000,
+        // refreshToken:"1//04p0AZ8i3e1kmCgYIARAAGAQSNwF-L9IrkY0BuCZzW1OQcneQ667kwAESpJFx9qaDujJ8UFn21vsCa11QuOdb7FTOux-6SdXUxMU",
+        // clientId:"510027605535-t2irpct15gan47pcb5hkpq5uce0gtbkh.apps.googleusercontent.com",
+        // clientSecret:"03iEMzRwIFnTflpu07DshyAR",
+        // accessUrl:"https://oauth2.googleapis.com/token"
         pass: "2123238t", // generated ethereal password
     },
 });
@@ -28,14 +39,15 @@ app.post('/sendMessage', async (req, res) => {
         from: "HR want me", // sender address
         to: "taniasilchenko212@gmail.com", // list of receivers
         subject: "Сообщение с Вашего портфолио", // Subject line
-        html: `<div><div><b>Name:</b>${name}</div>
-<b>message:</b>${message}
+          html: `<div><div><b>Name:</b>${name}</div>
+ <b>message:</b>${message}
 <div><b>contacts:</b>${contact}</div>
         </div>`, // html body
     });
     res.send("ok")
 });
 
-app.get('/', (req, res) => res.send('Hi!'));
-let port = process.env.PORT||3010;
+app.get('/', (req, res) => res.send('Hi people!'));
+
+// let port = "http://localhost:3010/";
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
