11df941eece73f43473c7bbf9320592a69ae3c9f
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-07-06 15:23:43 +0300

    fix: fix env variables

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
-let login = process.env.SMTP_LOGIN||"...";
-let password = process.env.SMTP_PASSWORD||"...";
-// let port = process.env.PORT||3010;
-let port = 3010;
+let smtp_login = process.env.SMTP_LOGIN||"...";
+let smtp_password = process.env.SMTP_PASSWORD||"...";
+
+let port = process.env.PORT||3010;
+
 
 app.use(cors());
 // parse application/x-www-form-urlencoded
@@ -22,14 +23,14 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         // type:"OAuth2",
-        user: "taniasilchenko212@gmail.com", // generated ethereal user
+        user: smtp_login, // generated ethereal user
         // accessToken:"ya29.a0AfH6SMDkjVwZEdSRd4Sh3WRPf3Q4N_umr99Un8FSRmzD9gSlhXsViga52spt2NTOpryy43TY_FU3sMB41fhtnwbDR3vvxvR2Ba3wxaGSM1dNvDY-ps3sJHPte2rabGypCfhb6JQ2C_ag-HqSXDqbby5ifsKVRCEBuik",
         // expires:1592570987208+60000,
         // refreshToken:"1//04p0AZ8i3e1kmCgYIARAAGAQSNwF-L9IrkY0BuCZzW1OQcneQ667kwAESpJFx9qaDujJ8UFn21vsCa11QuOdb7FTOux-6SdXUxMU",
         // clientId:"510027605535-t2irpct15gan47pcb5hkpq5uce0gtbkh.apps.googleusercontent.com",
         // clientSecret:"03iEMzRwIFnTflpu07DshyAR",
         // accessUrl:"https://oauth2.googleapis.com/token"
-        pass: "2123238t", // generated ethereal password
+        pass: smtp_password, // generated ethereal password
     },
 });
 
